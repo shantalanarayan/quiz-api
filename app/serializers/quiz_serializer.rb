c2e37bd61ecf3bbcfac31fb062b158b946cbbe10
shantalanarayan <mail2shantala@gmail.com>
2020-03-09 19:56:11 -0400

    Add Macros to Models AND Serializers Added macros to user and quiz

--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 class QuizSerializer < ActiveModel::Serializer
-  attributes :id, :topic
+  attributes :id, :topic, :editable
+  belongs_to :user
+
+  def editable
+    scope == object.user
+  end
 end
