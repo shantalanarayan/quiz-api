edcb4684304253779de938704a515e6be21a3024
shantalanarayan <mail2shantala@gmail.com>
2020-03-13 00:35:33 -0400

    Updated Model to delete related quizbanks on deletion of quiz Added dependent destroy

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,7 +2,7 @@
 
 class Quiz < ApplicationRecord
   belongs_to :user
-  has_many :quiz_banks
+  has_many :quiz_banks, dependent: :destroy
   accepts_nested_attributes_for :quiz_banks, allow_destroy: true
 
   validates :topic, :user, presence: true
